04f39ee0c9ab1c244243f59bfd9a8e38f7233c6a
Prathmesh-Posugade <prathmeshposugade@gmail.com>
2024-10-18 04:57:53 +1300

    17/10

--- Assignment0301.java
+++ Assignment0301.java
@@ -5,43 +5,47 @@ public class Invoice {
 	private String partDes;
 	private int quantity;
 	private double partPrice;
-	//static members can be initialized at time of declaration
-	//in reference s not any other objects reference is allowed to store due to final field
-	final static Scanner s=new Scanner(System.in);
-	
+	// static members can be initialized at time of declaration
+	// in reference s not any other objects reference is allowed to store due to
+	// final field
+	final static Scanner s = new Scanner(System.in);
+
 	static {
-		//static members can be initialized at static block(it execute only ones by jvm)		
+		// static members can be initialized at static block(it execute only ones by
+		// jvm)
 	}
-	
+
 	public Invoice() {
 		this("1", "undefined", 0, .0);
 	}
+
 	public Invoice(String partNumber, String partDes, int quantity, double partPrice) {
-		this.partDes=partDes;
-		this.partNumber=partNumber;
-		if(quantity<0) {
-			this.quantity=0;			
+		this.partDes = partDes;
+		this.partNumber = partNumber;
+		if (quantity < 0) {
+			this.quantity = 0;
+		} else {
+			this.quantity = quantity;
 		}
-		else {			
-			this.quantity=quantity;
-		}
-		if(partPrice<0) {
-			this.partPrice=.0;			
-		}
-		else {			
-			this.partPrice=partPrice;
+		if (partPrice < 0) {
+			this.partPrice = .0;
+		} else {
+			this.partPrice = partPrice;
 		}
 	}
-	
+
 	public void setPartDes(String partDes) {
 		this.partDes = partDes;
 	}
+
 	public void setPartNumber(String partNumber) {
 		this.partNumber = partNumber;
 	}
+
 	public void setPartPrice(double partPrice) {
 		this.partPrice = partPrice;
 	}
+
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
@@ -49,16 +53,19 @@ public class Invoice {
 	public String getPartDes() {
 		return partDes;
 	}
+
 	public String getPartNumber() {
 		return partNumber;
 	}
+
 	public double getPartPrice() {
 		return partPrice;
 	}
+
 	public int getQuantity() {
 		return quantity;
 	}
-	
+
 	public void accept() {
 		double n;
 		System.out.println("Enter part number: ");
@@ -66,49 +73,49 @@ public class Invoice {
 		System.out.println("Enter part description: ");
 		setPartDes(s.next());
 		System.out.println("Enter part quantity: ");
-		n=s.nextInt();
-		if(n<0) {
+		n = s.nextInt();
+		if (n < 0) {
 			setQuantity(0);
-		}
-		else {
-			setQuantity((int)n);
+		} else {
+			setQuantity((int) n);
 		}
 		System.out.println("Enter part price: ");
-		n=s.nextDouble();
-		if(n<0) {
+		n = s.nextDouble();
+		if (n < 0) {
 			setPartPrice(.0);
-		}
-		else {
-			setPartPrice(n);			
+		} else {
+			setPartPrice(n);
 		}
 	}
+
 	public void display() {
-		System.out.println(getPartNumber()+"\t\t"+getPartDes()+"\t\t"+getQuantity()+"\t\t"+getPartPrice());
+		System.out.println(getPartNumber() + "\t\t" + getPartDes() + "\t\t" + getQuantity() + "\t\t" + getPartPrice());
 	}
-	public static double calculateTotalBill(Invoice... i){
-		double total=.0;
-		for(Invoice o: i) {
-			total=total+(o.getPartPrice()*o.getQuantity());
+
+	public static double calculateTotalBill(Invoice... i) {
+		double total = .0;
+		for (Invoice o : i) {
+			total = total + (o.getPartPrice() * o.getQuantity());
 		}
 		return total;
 	}
-	
+
 }
 
 public class Assignment0301 {
 
 	public static void main(String[] args) {
-		Invoice i1=new Invoice();
-		Invoice i2=new Invoice();
-		Invoice i3=new Invoice();
+		Invoice i1 = new Invoice();
+		Invoice i2 = new Invoice();
+		Invoice i3 = new Invoice();
 		i1.accept();
 		i2.accept();
 		i3.accept();
-		System.out.println("getPartNumber()"+"\t"+"getPartDes()"+"\t"+"getQuantity()"+"\t"+"getPartPrice()");
+		System.out.println("getPartNumber()" + "\t" + "getPartDes()" + "\t" + "getQuantity()" + "\t" + "getPartPrice()");
 		i1.display();
 		i2.display();
 		i3.display();
-		System.out.println("Total Bill: "+Invoice.calculateTotalBill(i1, i2, i3));
+		System.out.println("Total Bill: " + Invoice.calculateTotalBill(i1, i2, i3));
 	}
 
 }
--- TestPoint.java
+++ TestPoint.java
@@ -3,52 +3,50 @@ import java.util.Scanner;
 public class Point2D {
 	private double x;
 	private double y;
-	final static Scanner s=new Scanner(System.in);
-	
+	final static Scanner s = new Scanner(System.in);
+
 	public Point2D() {
 		this(.0, .0);
 	}
-	
+
 	public Point2D(double a, double b) {
-		x=a;
-		y=b;
+		x = a;
+		y = b;
 	}
-	
+
 	public void acceptCood() {
 		System.out.println("Enter x and y: ");
-		x=s.nextDouble();
-		y=s.nextDouble();
+		x = s.nextDouble();
+		y = s.nextDouble();
 	}
-	
+
 	public String getDetails() {
-		return "x: "+x+" y: "+y;
+		return "x: " + x + " y: " + y;
 	}
-	
+
 	public boolean isEqual(Point2D o) {
-		if(x==o.x&&y==o.y) {
+		if (x == o.x && y == o.y) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	public double calculateDistance(Point2D d) {
-		return Math.sqrt(Math.pow((x-d.x), 2)+Math.pow((y-d.y), 2));
+		return Math.sqrt(Math.pow((x - d.x), 2) + Math.pow((y - d.y), 2));
 	}
 }
-
 
 public class TestPoint {
 
 	public static void main(String[] args) {
-		Point2D p=new Point2D();
-		Point2D p1=new Point2D();
+		Point2D p = new Point2D();
+		Point2D p1 = new Point2D();
 		p.acceptCood();
 		p1.acceptCood();
-		if(p.isEqual(p1)) {
-			System.out.println("Co-ordinates are equal: "+p.getDetails());
-		}
-		else {
-			System.out.println("Distance: "+p.calculateDistance(p1));
+		if (p.isEqual(p1)) {
+			System.out.println("Co-ordinates are equal: " + p.getDetails());
+		} else {
+			System.out.println("Distance: " + p.calculateDistance(p1));
 		}
 	}
 
